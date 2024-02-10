0b5d5c3c77594796fd10412671044ab8fccb2595
GURU PRASAATH RG <141542563+prasaathguru@users.noreply.github.com>
2024-02-10 17:03:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -16,36 +16,36 @@ def main():
             img = np.array(Image.open(uploaded_file))
             st.image(img)
             st.write("Processing..")
-            # text = pytesseract.image_to_string(img3)
-            # print(text)
-            # st.write(text)
+            text = pytesseract.image_to_string(img3)
+            print(text)
+            st.write(text)
             # Load the input image
 
 # Check if the image is loaded successfully
-            if img is None:
-                print("Error: Could not load the image.")
+            # if img is None:
+                # print("Error: Could not load the image.")
     # Add appropriate error handling or exit the program if necessary
 
 # Assuming 'img' is your input image
 
-            cv2norm_img = np.zeros((img.shape[0], img.shape[1]))
+            # cv2norm_img = np.zeros((img.shape[0], img.shape[1]))
 
 # Correct the variable name to cv2norm_img in the following line
-            img = cv2.normalize(img, cv2norm_img, 0, 255, cv2.NORM_MINMAX)
+            # img = cv2.normalize(img, cv2norm_img, 0, 255, cv2.NORM_MINMAX)
 
 # Convert the image to uint8 before using cv2.threshold
-            img = img.astype(np.uint8)
+            # img = img.astype(np.uint8)
 
 # Correct the thresholding line
-            _, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+    # _, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
 
 # Correct GaussianBlur parameters
-            img = cv2.GaussianBlur(img, (5, 5), 0)
+            # img = cv2.GaussianBlur(img, (5, 5), 0)
             
-            text = pytesseract.image_to_string(img)
-            print(text)
+            # text = pytesseract.image_to_string(img)
+            # print(text)
             
-            st.write(text)
+            # st.write(text)
             
             
             # if st.button("Stream data"):
