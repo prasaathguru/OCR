415b2c410b1bead5a9121fb673bf0349cd886e85
GURU PRASAATH RG <141542563+prasaathguru@users.noreply.github.com>
2024-02-10 16:55:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract'
 import numpy as np
 import cv2
 import time
